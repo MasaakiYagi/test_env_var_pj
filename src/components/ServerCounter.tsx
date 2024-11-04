41d511f65c8d3893c1db8afeebc962c38c27116a
YaGitHub <6135252+MasaakiYagi@users.noreply.github.com>
2024-11-04 21:50:15 +0900

    test initialIncrement

--- src/components/ServerCounter.tsx
+++ src/components/ServerCounter.tsx
@@ -19,6 +19,7 @@ export default function ServerCounter({ initialCount }: ServerCounterProps) {
   return (
     <div className="flex flex-col items-center bg-gray-100 p-6 rounded-lg shadow-md w-48 mx-auto my-4 text-center">
       <h2 className="text-xl font-semibold text-gray-800 mb-3">Server Side Counter</h2>
+      <p className="text-3xl font-bold text-blue-600 mb-4">{initialIncrement}</p>
       <p className="text-3xl font-bold text-blue-600 mb-4">{serverCount}</p>
       <p className="text-sm text-green-500 mb-2">{serverCounterText}</p>
       <p className="text-sm text-gray-500 mb-4">（リロードすると増加します）</p>
